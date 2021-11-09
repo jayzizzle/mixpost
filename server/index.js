63240fffde8e58a2473714f53bf4f800fab0824c
jayzizzle <dj.jonzamora@gmail.com>
2021-11-08 19:40:12 -0800

    add config for mongo

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const db = require('./config/keys').mongoURI;
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(db)
+  .then(() => console.log('Connected to MongoDB successfully'))
+  // .catch((err) => console.log(err));
+  .catch((err) => console.log('no connection'));
