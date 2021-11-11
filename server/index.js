b93fba04b2bbd248369cf709bf82760c8ce14259
jayzizzle <dj.jonzamora@gmail.com>
2021-11-10 19:20:08 -0800

    update user registration

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-app.get('/hello', (req, res) => {
-  res.send('hello world');
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/mixpost');
+
+app.post('/api/session', async (req, res) => {
+  console.log(req.body);
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json({ status: 'ok' });
+  } catch (error) {
+    res.json({ status: 'error', error: 'Username/Email already taken.' });
+  }
 });
 
 app.listen(5000, () => {
